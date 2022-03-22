1dc1d08ce34997534bf98baef50878ac16af6585
Arthur Gonçalves <arthur.r.goncalves@gmail.com>
2022-03-22 15:32:13 -0300

    fix Vitrine, html com modais

--- src/js/dashboardScript.js
+++ src/js/dashboardScript.js
@@ -1,16 +1,32 @@
 import { API } from "./model/API.js";
+import { Vitrine } from "./model/Vitrine.js";
 
-console.log(API.products())
+Vitrine.createAdminPageProducts()
 
-export class Filtros {
+
+
+class Filtros {
 
     static async filtrarNome(input){
         
         const produtos = await API.products()
 
-        let pesquisa = paises.filter(produto=>produto.name == input.value)
+        let pesquisa = produtos.filter(produto=>produto.name == input)
 
         return pesquisa
     }
     
 }
+
+console.log(Filtros.filtrarNome())
+
+const campoBuscaNome = document.querySelector("#pesqNome")
+campoBuscaNome.addEventListener("keyup", function(){
+
+    const value = campoBuscaNome.value
+    console.log(value)
+    
+    let resposta = Filtros.filtrarNome(value)
+
+    console.log(resposta)
+})
