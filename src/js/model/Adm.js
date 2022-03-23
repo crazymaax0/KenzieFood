fe1d5c4cd5009cba8fa0561ba05b0ec12b3f31a6
Max Milan Campos de Oliveira <95248203+crazymaax@users.noreply.github.com>
2022-03-23 19:37:58 -0300

    Funções para logar e registrar.

--- src/js/model/Adm.js
+++ src/js/model/Adm.js
@@ -10,6 +10,83 @@ export class Adm {
         descricao: ''
     };
 
+    static getUserInfo(){
+        const colectInfo = window.localStorage.getItem("userData") 
+        return JSON.parse(colectInfo)
+    }
+
+    static setUserInfo(value){
+        const dataJSON = JSON.stringify(value)
+        window.localStorage.setItem("userInfo", dataJSON)
+    }
+
+    static async login(e){
+        e.preventDefault()
+
+        let loginForm = e.target.parentNode
+
+        const userInfos = {}
+
+        for(let i = 0; i < loginForm.length -1; i++){
+            if(loginForm[i].name){
+                console.log(loginForm[i].name)
+                const name = loginForm[i].name
+                const value = loginForm[i].value
+
+                userInfos[name] = value
+            }
+
+            loginForm[i].value = ""
+        }
+        console.log(userInfos)
+        const loginToken = await API.login(userInfos)
+
+        if(loginToken === 404){
+            /* CRIAR VIA DOM UM POPUP OU MENSAGEM QUE DIZ QUE DEU RUIM */
+        }else{
+
+            Adm.setUserInfo(loginToken)
+
+            if(loginToken === API.token){
+                window.location = "../../../dashboard.html"
+            }else{
+                window.location = "../../../index.html"
+            }
+        }
+        
+    }
+
+    static async register(e){
+        e.preventDefault()
+
+        let registerForm = e.target.parentNode
+        console.log(registerForm)
+        const userInfos = {}
+
+        for(let i = 0; i < registerForm.length -1; i++){
+            if(registerForm[i].name){
+                console.log(registerForm[i].name)
+                const name = registerForm[i].name
+                const value = registerForm[i].value
+
+                userInfos[name] = value
+            }
+
+            registerForm[i].value = ""
+        }
+        console.log(userInfos)
+        const registerToken = await API.register(userInfos)
+
+        if(registerToken === 404){
+            /* CRIAR VIA DOM UM POPUP OU MENSAGEM QUE DIZ QUE DEU RUIM */
+        }else{
+
+            Adm.setUserInfo(registerToken)
+            /* O USUARIO FOI CRIADO, MOSTRAR MENSAGEM QUE FOI CRIADO E MUDAR O SELECIONADO PARA LOGIN */
+        }
+        
+    }
+
     static getInfosNewProduct(){
 
         const form = document.getElementById('cadastroProduto');
