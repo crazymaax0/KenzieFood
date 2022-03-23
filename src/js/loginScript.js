5801fb149e78e80b0f8b819dd27d6f956d90e469
IgorTenoso <lucasmirandaneves@gmail.com>
2022-03-22 23:59:30 -0300

    login page on

--- src/js/loginScript.js
+++ src/js/loginScript.js
@@ -1,1 +1,23 @@
 import { API } from "./model/API.js";
+
+const login        = document.getElementById("login")
+const register     = document.getElementById("register")
+
+const loginForm    = document.getElementById("loginForm")
+const registerForm = document.getElementById("registerForm")
+
+login.addEventListener("click", () => {
+  register.classList.remove("color")
+  login.classList.add("color")
+
+  loginForm.classList.remove("displayNone")
+  registerForm.classList.add("displayNone")
+})
+
+register.addEventListener("click", () => {
+  login.classList.remove("color")
+  register.classList.add("color")
+
+  registerForm.classList.remove("displayNone")
+  loginForm.classList.add("displayNone")
+})
