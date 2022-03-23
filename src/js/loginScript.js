f18fb5c1f444e4c70a56c59e1e025a45aa47deb1
Max Milan Campos de Oliveira <95248203+crazymaax@users.noreply.github.com>
2022-03-23 19:37:25 -0300

    Botões dos formularios.

--- src/js/loginScript.js
+++ src/js/loginScript.js
@@ -1,3 +1,4 @@
+import { Adm } from "./model/Adm.js";
 import { API } from "./model/API.js";
 
 const login        = document.getElementById("login")
@@ -5,6 +6,13 @@ const register     = document.getElementById("register")
 
 const loginForm    = document.getElementById("loginForm")
 const registerForm = document.getElementById("registerForm")
+
+const loginButton  = loginForm.children[2]
+const registerButton = registerForm.children[3]
+
+loginButton.addEventListener("click", Adm.login)
+registerButton.addEventListener("click", Adm.register)
+
 
 login.addEventListener("click", () => {
   register.classList.remove("color")
