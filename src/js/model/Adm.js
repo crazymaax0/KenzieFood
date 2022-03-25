e00006cad44eb51cc1a6d252848efa89dd8bc6f8
IgorTenoso <lucasmirandaneves@gmail.com>
2022-03-24 23:30:37 -0300

    Update Adm.js

--- src/js/model/Adm.js
+++ src/js/model/Adm.js
@@ -10,11 +10,93 @@ export class Adm {
         descricao: ''
     };
 
+<<<<<<< Updated upstream
     static dataRegisterAdmin = {
         name: '',
         email: '',
         password: ''
     };
+=======
+    static getUserInfo(){
+        const colectInfo = window.localStorage.getItem("userInfo") 
+        return JSON.parse(colectInfo)
+    }
+
+    static setUserInfo(value){
+        const dataJSON = JSON.stringify(value)
+        window.localStorage.setItem("userInfo", dataJSON)
+    }
+
+    static async login(e){
+        e.preventDefault()
+
+        let loginForm = e.target.parentNode
+
+        const userInfos = {}
+
+        for(let i = 0; i < loginForm.length -1; i++){
+            if(loginForm[i].name){
+
+                const name = loginForm[i].name
+                const value = loginForm[i].value
+
+                userInfos[name] = value
+            }
+
+            loginForm[i].value = ""
+        }
+
+        const loginToken = await API.login(userInfos)
+
+
+        if(loginToken === 404 || loginToken === 401){
+        }else{
+
+            Adm.setUserInfo(loginToken)
+
+            if(userInfos.email === 'equipe4@gmail.com'){
+
+                const dataAdmin = {
+                    email: 'equipe4@gmail.com',
+                    senha: 123
+                }
+
+                localStorage.setItem('infosAdmin', JSON.stringify(dataAdmin))
+
+                window.location = "../../../api-kenziefood-m2-mucasliranda/index.html"
+            }else{
+                window.location = "../../../api-kenziefood-m2-mucasliranda/index.html"
+            }
+        }
+        
+    }
+
+    static async register(e){
+        e.preventDefault()
+
+        let registerForm = e.target.parentNode
+        const userInfos = {}
+
+        for(let i = 0; i < registerForm.length -1; i++){
+            if(registerForm[i].name){
+                console.log(registerForm[i].name)
+                const name = registerForm[i].name
+                const value = registerForm[i].value
+
+                userInfos[name] = value
+            }
+
+            registerForm[i].value = ""
+        }
+        const registerToken = await API.register(userInfos)
+
+        if(registerToken === 404){
+        }else{
+            Adm.setUserInfo(registerToken)
+        }
+        
+    }
+>>>>>>> Stashed changes
 
     static getInfosNewProduct(){
 
